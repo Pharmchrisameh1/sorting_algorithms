58099c4dd79bc7047d8d5ea41dcec8a0e8252299
Victorjoseph93 <139341700+Victorjoseph93@users.noreply.github.com>
2024-10-29 08:32:49 +0100

    Update 1-insertion_sort_list.c

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -3,9 +3,9 @@
 /**
  * insertion_sort_list - This function sorts a doubly linked list using the
  * insertion sort algorithm.
- * @list: a double pointer to the head of the doubly linked list.
+ * @list: the double pointer to the head of the doubly linked list.
  *
- * Return: void.
+ * Return: Returns void.
  */
 void insertion_sort_list(listint_t **list)
 {
